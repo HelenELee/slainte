9e6749e7f441245489505c9c171f947bedcef8f9
HelenELee <HelenELee3@outlook.com>
2023-04-28 21:44:30 +0800

    restructure directory

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+//127.0.0.1:27017
+//localhost
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/me-time', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //useCreateIndex: true,
+ // useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
