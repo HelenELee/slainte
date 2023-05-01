4dd2236b9859ea541a5be7b9ce141a1699cd5eb8
HelenELee <HelenELee3@outlook.com>
2023-05-01 21:24:47 +0800

    console logs

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -34,6 +34,7 @@ class AuthService {
 
   login(idToken) {
     // Saves user token to localStorage
+    console.log("LOGIN - " + idToken);
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
