163e56793e93bf50226b913b7ade8db946f558a4
HelenELee <HelenELee3@outlook.com>
2023-05-03 16:41:03 +0800

    added seeding for activities

---
+++ server/seeds/seed.js
@@ -0,0 +1,18 @@
+const db = require('../config/connection');
+const  { Activity } = require('../models');
+const  activitySeeds = require('./activities.json');
+
+db.once('open', async () => {
+    try {
+      await Activity.deleteMany({});
+      
+      await Activity.create(activitySeeds);
+  
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  
+    console.log('all done!');
+    process.exit(0);
+})
