f019c9df4b43bf31ce38bb4d3a9d613879de7c69
HelenELee <HelenELee3@outlook.com>
2023-05-10 20:06:04 +0800

    validation on forms

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
-//import { Form, Button, Alert } from 'react-bootstrap';
+
 import { StyledForm, StyledInput, StyledButton, StyledAlert, StyledLabel } from './FormComponents';
-import { StyledModal } from "./StyledModal"
-//import { loginUser } from '../utils/API';
+import { validateEmail, checkPassword } from '../utils/helpers';
 
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
@@ -12,9 +11,16 @@ import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  //const [validated] = useState(false);
- // const [showAlert, setShowAlert] = useState(false);
   const [login, { error, data }] = useMutation(LOGIN_USER);
+ 
+  const isValidEmail = userFormData.email !== "" && validateEmail(userFormData.email);
+  const isValidPassword = userFormData.password !== "" && checkPassword(userFormData.password);
+  
+
+  const [enteredPassword, setEnteredPassword] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState(false);
+
+  const [submitClicked, setSubmitClicked] = useState(false);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -23,13 +29,7 @@ const LoginForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    //if (form.checkValidity() === false) {
-    //  event.preventDefault();
-    //  event.stopPropagation();
-    //}
+    
     try {
       const { data } = await login({
         variables: { ...userFormData },
@@ -41,11 +41,11 @@ const LoginForm = () => {
     }
 
 
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
+    // setUserFormData({
+    //   username: '',
+    //   email: '',
+    //   password: '',
+    // });
   };
 
   return (
@@ -53,10 +53,29 @@ const LoginForm = () => {
     
       <StyledForm onSubmit={handleFormSubmit}>
             <StyledLabel>Email:</StyledLabel>
-            <StyledInput type="text" value={userFormData.email} onChange={handleInputChange} name="email" placeholder="Your email" required/>
-            <StyledLabel >Password:</StyledLabel>
-            <StyledInput type="password" value={userFormData.password} onChange={handleInputChange} name="password" placeholder="Your password" />
+            <StyledInput 
+            type="text" 
+            value={userFormData.email} 
+            onChange={handleInputChange} 
+            onBlur={() => setEnteredEmail(true)}
+            name="email" 
+            placeholder="Your email" 
+            required
+            size="80%"
+            />
             
+            <StyledLabel >Password:</StyledLabel>
+            <StyledInput 
+            type="password" 
+            value={userFormData.password} 
+            onChange={handleInputChange} 
+            onBlur={() => setEnteredPassword(true)}
+            name="password" 
+            placeholder="Your password"
+            size="80%"
+             />
+            {enteredPassword && enteredEmail ? (isValidPassword && isValidEmail ? "" : <p>❌ Please enter valid details.</p>) : null}
+            <br />
             <StyledButton type="submit" disabled={!userFormData.email || !userFormData.password}>Login</StyledButton>
       
         </StyledForm>
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,47 +1,69 @@
 import React, { useState } from 'react';
-//import { Form, Button, Alert } from 'react-bootstrap';
 import { StyledForm, StyledInput, StyledButton, StyledAlert, StyledLabel } from './FormComponents';
-//import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
-
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations'
-
+import { validateEmail, checkPassword } from '../utils/helpers';
 
 const SignupForm = (props) => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  // set state for form validation
- // const [validated] = useState(false);
-  // set state for alert
-  //const [showAlert, setShowAlert] = useState(false);
+
   const [addUser, { error, data }] = useMutation(ADD_USER);
 
+  
+  //const isValidPassword = userFormData.password !== "" && checkPassword(userFormData.password);
+
+  //const [enteredPassword, setEnteredPassword] = useState(false);
+  // [enteredEmail, setEnteredEmail] = useState(false);
+  const [enteredDetails, setEnteredDetails] = useState(false);
+  const [isValidName, setIsValidName] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+
   const handleInputChange = (event) => {
+    console.log("********INPUT CHANGE", event.target.name);
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
+    console.log("UserFormData", userFormData);
+    if (event.target.name === "email") {
+      if (userFormData.email !== "" && validateEmail(userFormData.email)) {
+        console.log("got here!!!");
+        setIsValidEmail(true);
+      } else {
+        setIsValidEmail(false);
+      }
+      //const isValidEmail = userFormData.email !== "" && validateEmail(userFormData.email);
+    } else if (event.target.name === "password") {
+      if (userFormData.password !== "" && checkPassword(userFormData.password)) {
+        setIsValidPassword(true);
+      } else {
+        setIsValidPassword(false);
+      }
+    } else {
+      if (userFormData.username !== "") {
+        setIsValidName(true);
+      }
+    }
+
+    if (userFormData.username !== "" && userFormData.email !== "" && userFormData.password !== "") {
+      setEnteredDetails(true);
+    }
+    console.log("VALID EMAIL", isValidEmail);
+    console.log("VALID PASSWORD", isValidPassword);
+    console.log("NAME", isValidName);
   };
 
   const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    // check if form has everything (as per react-bootstrap docs)
-    //const form = event.currentTarget;
-    // if (form.checkValidity() === false) {
-    //   event.preventDefault();
-    //   event.stopPropagation();
-    // }
-   
+    event.preventDefault();   
     
     try {
       console.log("USER DATA", userFormData);
       const { data } = await addUser({
         variables: { ...userFormData },
       });
-      console.log("ABOUT TO DO AUTH");
+      
       Auth.login(data.addUser.token);
-      console.log("****TOKEN********")
-      console.log(data.addUser.token);
 
     } catch (e) {
       console.error(e);
@@ -60,14 +82,39 @@ const SignupForm = (props) => {
     <>
       <StyledForm onSubmit={handleFormSubmit}>
             <StyledLabel>Username:</StyledLabel>
-            <StyledInput type="text" value={userFormData.username} onChange={handleInputChange} name="username" placeholder="Your username" required/>
+            <StyledInput 
+            type="text" 
+            value={userFormData.username} 
+            onChange={handleInputChange} 
+            onBlur={() => setIsValidName(true)}
+            name="username" 
+            placeholder="Your username" 
+            required/>
           
             <StyledLabel>Email:</StyledLabel>
-            <StyledInput type="text" value={userFormData.email} onChange={handleInputChange} name="email" placeholder="Your email" required/>
+            <StyledInput 
+            type="text" 
+            value={userFormData.email} 
+            onChange={handleInputChange} 
+            onBlur={handleInputChange}
+            //onBlur={() => setEnteredEmail(true)}
+            name="email" 
+            placeholder="Your email" 
+            required/>
             <StyledLabel >Password:</StyledLabel>
-            <StyledInput type="password" value={userFormData.password} onChange={handleInputChange} name="password" placeholder="Your password" />
+            <StyledInput 
+            type="password" 
+            value={userFormData.password} 
+            onChange={handleInputChange} 
+            //onBlur={() => setEnteredPassword(true)}
+            name="password" 
+            placeholder="Your password" />
             
-            <StyledButton type="submit" disabled={!userFormData.username || !userFormData.password}>Sign up</StyledButton>
+            {enteredDetails && (enteredDetails && isValidPassword && isValidEmail && isValidName ? "" : <p>❌ Please enter valid details.</p>)}
+            <StyledButton 
+            type="submit" 
+            disabled={!isValidName || !isValidPassword || !isValidEmail}
+            >Sign up</StyledButton>
       
         </StyledForm>
     </>
