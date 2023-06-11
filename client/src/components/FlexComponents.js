cf32d980e4743e703f7bd2885bf6c86d1daf95c0
HelenELee <HelenELee3@outlook.com>
2023-06-11 20:12:55 +0800

    added media query

--- client/src/components/FlexComponents.js
+++ client/src/components/FlexComponents.js
@@ -8,10 +8,17 @@ export const FlexContainer = styled.div`
 	justify-content: center;
 	align-items: stretch;
 	align-content: stretch;
+	margin-bottom: 20px;
+	@media (max-width: 1000px) {
+		flex-direction: ${props => props.directionSM ? props.directionSM : props.direction};
+	}
 `
 
 export const FlexChild = styled.div`
 	flex: 1 1 20%;
+	// border: solid 1px;
+	margin: ${props => props.margin ? props.margin : ''};
+	// margin: auto;
 	
 `
 
