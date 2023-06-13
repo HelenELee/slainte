19fbae5aceac2f947a8ce17d47d75b9470b0f456
HelenELee <HelenELee3@outlook.com>
2023-06-13 20:43:03 +0800

    added validation for date, checkIsValidDate, error message, isValidDate based on dayId

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,3 +1,4 @@
+//helper functions used for validation
 export function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
@@ -15,3 +16,17 @@ export function checkPassword(input) {
 export function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
+
+//const { enAULocale } = require('date-fns/locale/en-AU');
+const { isFuture } = require('date-fns');
+
+//used in DayForm to ensure date entered is not in the future
+export function checkIsValidDate(dateString) {
+  let enteredDate = new Date(dateString)
+  if (isFuture(enteredDate)){
+    return false;
+  } else {
+    return true;
+  }
+}
+
